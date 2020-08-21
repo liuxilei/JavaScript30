f2c591d401a57bed37902262a0555654947f08e5
liuxilei <liuxileioo@qq.com>
2020-08-21 14:54:16 +0800

    feat: video部分

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,74 @@
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+function togglePlay() {
+    const method = video.paused ? "play" : "pause";
+    video[method]();
+}
+
+function updateButton() {
+    const icon = this.paused ? '▶' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+//进入全屏
+function FullScreen(ele) {
+    if (ele.requestFullscreen) {
+        ele.requestFullscreen();
+    } else if (ele.mozRequestFullScreen) {
+        ele.mozRequestFullScreen();
+    } else if (ele.webkitRequestFullScreen) {
+        ele.webkitRequestFullScreen();
+    }
+}
+//退出全屏
+function exitFullscreen(de) {
+    if (de.exitFullscreen) {
+        de.exitFullscreen();
+    } else if (de.mozCancelFullScreen) {
+        de.mozCancelFullScreen();
+    } else if (de.webkitCancelFullScreen) {
+        de.webkitCancelFullScreen();
+    }
+}
+
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+toggle.addEventListener("click", togglePlay);
+skipButtons.forEach(button => button.addEventListener("click", skip));
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+
+let mouseDown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mouseDown && scrub(e));
+progress.addEventListener("mousedown", () => mouseDown = true);
+progress.addEventListener("mouseup", () => mouseDown = false);
+
+const fullScreen = document.querySelector(".fullScreen");
+fullScreen.addEventListener("click", () => FullScreen(video));
